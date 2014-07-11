75b03bf608696cb906de41d099f55aa110454f74
JavierReyesO <javier.reyes.12@sansano.usm.cl>
2014-07-11 01:37:49 -0400

    suficiente

--- editarAdmin.php
+++ editarAdmin.php
@@ -1,3 +1,28 @@
 <?php
 
->
+$con=pg_connect("host=localhost port=5432 user=postgres password=BD dbname=BD");
+if(!$con) {echo "Conexion no establecida, verifique sus datos";}
+
+$nombre=$_POST["nombre"];
+$rut = $_POST["rut"];
+$rol = $_POST["rol"];
+$telefono = $_POST["telefono"];
+$talla = $_POST["talla"];
+$carrera = $_POST["carrera"];
+
+
+$SQL2= "SELECT rol, student_id FROM students, coordinators WHERE students.rol='$rol' AND students.id = coordinators.student_id";
+$consulta = pg_query($con, $SQL2);
+
+$compara=pg_fetch_array($busqueda);
+$student_id = intval($compara["student_id"]);
+
+$SQL3 = " UPDATE coordinators SET talla='$talla' WHERE student_id=1";
+$consulta3 = pg_query($con, $SQL3);
+
+$SQL = "UPDATE students SET nombre = '$nombre', rut = '$rut', rol='$rol', telefono='$telefono', carrera= '$carrera' WHERE rol='$rol'";
+$consulta2= pg_query($con, $SQL);
+
+header('Location: misdatos.php');
+
+?>
--- misdatos.php
+++ misdatos.php
@@ -10,11 +10,11 @@ if(!$con)
 }
 
 
-$SQL= "SELECT * FROM students WHERE rol='$rol'";
+$SQL= "SELECT nombre, rol, rut, student_id, telefono, carrera, talla FROM students, coordinators WHERE rol='$rol' AND students.id=coordinators.student_id";
 $busqueda=pg_query($SQL);
 $compara=pg_fetch_array($busqueda);
 $numrows = pg_numrows($busqueda);
-$talla="";
+$talla = $compara["talla"];
 
 ?>
 
@@ -36,37 +36,37 @@ $talla="";
     <tr>
         <td>Nombre</td>
         <td>
-            <input type="text" name="nombrenuevo" value="<?php echo $compara["nombre"] ?>"/>
+            <input type="text" name="nombre" value="<?php echo $compara["nombre"] ?>"/>
         </td>
     </tr>
     <tr>
         <td>Rol USM</td>
         <td>
-            <input type="text" name="colaboradores" value="<?php echo $compara["rol"] ?>"/>
+            <input type="text" name="rol" value="<?php echo $compara["rol"] ?>"/>
         </td>
     </tr>
     <tr>
         <td>Rut</td>
         <td>
-            <input type="text" name="colaboradores" value="<?php echo $compara["rut"] ?>"/>
+            <input type="text" name="rut" value="<?php echo $compara["rut"] ?>"/>
         </td>
     </tr>
     <tr>
         <td>Teléfono</td>
         <td>
-            <input type="text" name="colaboradores" value="<?php echo $compara["telefono"] ?>"/>
+            <input type="text" name="telefono" value="<?php echo $compara["telefono"] ?>"/>
         </td>
     </tr>
     <tr>
         <td>Talla</td>
         <td>
-            <input type="text" name="colaboradores" value="<?php echo $talla ?>"/>
+            <input type="text" name="talla" value="<?php echo $talla ?>"/>
         </td>
     </tr>
     <tr>
         <td>Carrera</td>
         <td>
-            <input type="text" name="colaboradores" value="<?php echo $compara["carrera"] ?>"/>
+            <input type="text" name="carrera" value="<?php echo $compara["carrera"] ?>"/>
         </td>
     </tr>
     <tr>
